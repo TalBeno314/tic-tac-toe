2d36b46a1b22daaa9db4e4f699b99530a020d334
טל בנו <talbeno123@gmail.com>
2020-11-09 13:51:38 +0200

    adding comments

--- sketch.js
+++ sketch.js
@@ -7,8 +7,10 @@ function setup() {
     cell = width / 3;
     offset = width / 20;
 }
+
 let turn = 0;
 
+//creating an empty board
 let board = [
     ['', '', ''],
     ['', '', ''],
@@ -19,11 +21,14 @@ function draw() {
     background(256);
     strokeWeight(6)
     stroke(0);
+
+    //drawing the board
     for (let i = 1; i <= 3; i++) {
         line(i * width / 3, 0, i * width / 3, width);
         line(0, i * width / 3, height, i * width / 3);
     }
 
+    //drawing X and O
     for (let i = 0; i <= 2; i++) {
         for (let j = 0; j <= 2; j++) {
             switch (board[i][j]) {
@@ -38,37 +43,46 @@ function draw() {
         }
     }
 
+    //calling a function to check if the game is over
+    //true, indicates that it is the main board
     gameOver(board, true);
 }
 
 function mouseClicked() {
-    console.log(turn);
+    //calculating click position on board
     let j = floor(mouseY / cell);
     let i = floor(mouseX / cell);
     if ((turn % 2 == 0) && !gameOver(board)[0]) {
         if (board[i][j] == '') {
+            //addin the X to the board
             board[i][j] = 'X';
             turn++;
         }
 
+        //calling the function to AI's turn.
         ai();
     }
 }
 
 
 function gameOver(board, isReal) {
+    //a function to check if the game is over
     stroke(255, 0, 0);
     strokeWeight(6);
     let game = [false, 'tie'];
     for (let i = 0; i <= 2; i++) {
         if (board[i][0] === board[i][1] && board[i][0] === board[i][2] && (board[i][0] === 'X' || board[i][0] === 'O')) {
-            game[0] = true;
-            game[1] = board[i][0];
+            //cehcling horizontal
+            game[0] = true; //telling that the game is over
+            game[1] = board[i][0]; //telling who won
             if (isReal) {
+                //if the board is the main board
+                //this will draw lies over the three winning cells.
                 line(i * cell + cell / 2, offset, i * cell + cell / 2, width - offset);
                 break;
             }
         } else if (board[0][i] === board[1][i] && board[0][i] === board[2][i] && (board[0][i] === 'X' || board[0][i] === 'O')) {
+            //checking vertical
             game[0] = true;
             game[1] = board[0][i];
             if (isReal) {
@@ -78,6 +92,7 @@ function gameOver(board, isReal) {
         }
     }
 
+    //cecking diagonals
     if (board[1][1] === board[0][0] && board[1][1] === board[2][2] && (board[1][1] === 'X' || board[1][1] === 'O')) {
         game[0] = true;
         game[1] = board[1][1];
@@ -92,13 +107,18 @@ function gameOver(board, isReal) {
         }
     }
 
+    //returning if the game is over, and who won.
+    //if no one won, the function will return [false, 'tie'];
     return game;
 }
 
 function ai() {
     if (!gameOver(board, false)[0]) {
+        //calling the minimax function
+        //minimax returns an array, [score of move, [move]]
         let move = minimax(board, 8 - turn, true)[1];
 
+        //addin O to the board, using the move given by minimax
         board[move[0]][move[1]] = 'O';
         turn++;
     }
@@ -106,6 +126,7 @@ function ai() {
 
 function minimax(board, depth, maximizing) {
     if (depth == -1 || gameOver(board, false)[0]) {
+        //if the board is full, or the game is over, the function will return depends on who wins.
         let score;
         if (gameOver(board, false)[1] == 'X') {
             score = -1;
@@ -124,35 +145,47 @@ function minimax(board, depth, maximizing) {
         for (let i = 0; i <= 2; i++) {
             for (let j = 0; j <= 2; j++) {
                 if (board[i][j] == '') {
-                    let next = clone(board);
-                    next[i][j] = 'O';
-                    let eval = minimax(next, depth - 1, false);
+                    //going through every empty cell on the board
+                    let next = clone(board); //creating a copy of the board
+                    next[i][j] = 'O'; //adding O to the empty cell
+                    let eval = minimax(next, depth - 1, false); //calling minimax on the child.
                     if (eval > maxValue) {
+                        //if the value of the child is bigger than the max value
+                        //the max value will be replaced
                         maxValue = eval;
+                        //saving the move that leads to max value
                         move = [i, j];
                     }
                 }
             }
         }
+        //returning the value and the move captured
         return [maxValue, move];
     } else {
         let minValue = +Infinity;
         for (let i = 0; i <= 2; i++) {
             for (let j = 0; j <= 2; j++) {
                 if (board[i][j] == '') {
-                    let next = clone(board);
-                    next[i][j] = 'X';
-                    let eval = minimax(next, depth - 1, true)[0];
+                    //going through every empty cell of the board
+                    let next = clone(board); //creating a copy
+                    next[i][j] = 'X'; //adding 'X' to the empty cell
+                    let eval = minimax(next, depth - 1, true)[0]; //calling minimax on the child
                     if (eval < minValue) {
+                        //replacing minimum value with a new, smaller one.
+                        //no need to save move on this one
+                        //as this is not the move the AI would do.
                         minValue = eval;
                     }
                 }
             }
         }
+        //returning the maximum value
         return minValue;
     }
 }
 
+//a function that clones an array
+//because "array = ogArray" would make a pointer and not clone it.
 function clone(board) {
     var newArray = [];
 
@@ -161,14 +194,4 @@ function clone(board) {
     }
 
     return newArray;
-}
-
-function reset() {
-    board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-
-    turn = 0;
 }
